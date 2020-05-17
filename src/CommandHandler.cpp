5fe409d6f94a7b9194860c954ac09e3476dd6391
ahmad pourihosseini <seyedahmadpourihosseini@yahoo.com>
2020-05-17 16:17:29 +0430

    Refactor CommandHandler to make conditionals inside runCommand shorter

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -24,38 +24,47 @@ void CommandHandler::runCommand(const std::string &command)
   try
   {
     const RequestType request(command);
-    if (request.getRequestUrl()[0] == "signup" && request.getMethod() == RequestType::Methods::POST)
-      interface.runSignupCommand(request);
-    else if (request.getRequestUrl()[0] == "login" && request.getMethod() == RequestType::Methods::POST)
-      interface.runLoginCommand(request);
-    else if (request.getRequestUrl()[0] == "logout" && request.getMethod() == RequestType::Methods::POST)
-      interface.runLogoutCommand(request);
-    else if (request.getRequestUrl()[0] == "wallet" && request.getMethod() == RequestType::Methods::POST)
-      interface.runAddWalletCommand(request);
-    else if (request.getRequestUrl()[0] == "wallet" && request.getMethod() == RequestType::Methods::GET)
-      interface.runGetWalletCommand(request);
-    else if (request.getRequestUrl()[0] == "hotels" && request.getMethod() == RequestType::Methods::GET)
-      interface.runGetHotelsCommand(request);
-    else if (request.getRequestUrl()[0] == "filters" && request.getMethod() == RequestType::Methods::POST)
-      interface.runAddFilterCommand(request);
-    else if (request.getRequestUrl()[0] == "filters" && request.getMethod() == RequestType::Methods::DELETE)
-      interface.runResetFilterCommand(request);
-    else if (request.getRequestUrl()[0] == "comments" && request.getMethod() == RequestType::Methods::POST)
-      interface.runAddCommentCommand(request);
-    else if (request.getRequestUrl()[0] == "comments" && request.getMethod() == RequestType::Methods::GET)
-      interface.runGetCommentsCommand(request);
-    else if (request.getRequestUrl()[0] == "ratings" && request.getMethod() == RequestType::Methods::GET)
-      interface.runGetRateCommand(request);
-    else if (request.getRequestUrl()[0] == "ratings" && request.getMethod() == RequestType::Methods::POST)
-      interface.runAddRateCommand(request);
-    else if (request.getRequestUrl()[0] == "reserves" && request.getMethod() == RequestType::Methods::POST)
-      interface.runSetReserveCommand(request);
-    else if (request.getRequestUrl()[0] == "reserves" && request.getMethod() == RequestType::Methods::GET)
-      interface.runGetReserveCommand(request);
-    else if (request.getRequestUrl()[0] == "reserves" && request.getMethod() == RequestType::Methods::DELETE)
-      interface.runDeleteReserveCommand(request);
-    else if (request.getRequestUrl()[0] == "sort" && request.getMethod() == RequestType::Methods::POST)
-      interface.runSortCommand(request);
+    
+    if (request.getMethod() == RequestType::Methods::GET) {
+      if (request.getRequestUrl()[0] == "wallet")
+        interface.runGetWalletCommand(request);
+      else if (request.getRequestUrl()[0] == "hotels")
+        interface.runGetHotelsCommand(request);
+      else if (request.getRequestUrl()[0] == "comments")
+        interface.runGetCommentsCommand(request);
+      else if (request.getRequestUrl()[0] == "ratings")
+        interface.runGetRateCommand(request);
+      else if (request.getRequestUrl()[0] == "reserves")
+        interface.runGetReserveCommand(request);
+    }
+
+    else if (request.getMethod() == RequestType::Methods::POST) {
+      if (request.getRequestUrl()[0] == "signup")
+        interface.runSignupCommand(request);
+      else if (request.getRequestUrl()[0] == "login")
+        interface.runLoginCommand(request);
+      else if (request.getRequestUrl()[0] == "logout")
+        interface.runLogoutCommand(request);
+      else if (request.getRequestUrl()[0] == "wallet")
+        interface.runAddWalletCommand(request);
+      else if (request.getRequestUrl()[0] == "filters")
+        interface.runAddFilterCommand(request);
+      else if (request.getRequestUrl()[0] == "comments")
+        interface.runAddCommentCommand(request);
+      else if (request.getRequestUrl()[0] == "ratings")
+        interface.runAddRateCommand(request);
+      else if (request.getRequestUrl()[0] == "reserves")
+        interface.runSetReserveCommand(request);
+      else if (request.getRequestUrl()[0] == "sort")
+        interface.runSortCommand(request);
+    }
+
+    else if (request.getMethod() == RequestType::Methods::DELETE) {
+      if (request.getRequestUrl()[0] == "filters")
+        interface.runResetFilterCommand(request);
+      else if (request.getRequestUrl()[0] == "reserves")
+        interface.runDeleteReserveCommand(request);
+    }
 
     else
       throw new NotFoundException();
